53ca563f728a4f6cf2cccf67a6db987288a24081
Best Verie Iradukunda <60257249+Best-Verie@users.noreply.github.com>
2021-09-23 13:12:18 +0200

    project setup

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location'
 
 export default function App() {
+
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() =>{
+
+    console.log("hey")
+    const load = async () =>{
+      try{
+    console.log("hey2")
+
+        let {status} =  Location.requestForegroundPermissionsAsync()
+        if(status !== 'granted'){
+    console.log("hey5")
+
+          setErrorMessage('Access to location is needed to run the app')
+          return
+        }
+        console.log("hlo")
+
+        const location =  Location.getCurrentPositionAsync()
+        const {latitude, longitude} = location.coords
+        alert(`latitude: ${latitude}, longitude: ${longitude}` )
+        console.log(`latitude: ${latitude}, longitude: ${longitude}`)
+        
+
+      }catch(err){
+        console.log(err.message)
+      }
+
+    }
+
+    load()
+  }, [])
+
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Bonjour Elois!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +50,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ecb202',
     alignItems: 'center',
     justifyContent: 'center',
   },
