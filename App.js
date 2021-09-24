640a33c2719c04f2d04bab9d177351efc7cea426
Best Verie Iradukunda <60257249+Best-Verie@users.noreply.github.com>
2021-09-24 18:01:50 +0200

    started fetching data from an open weather api

--- App.js
+++ App.js
@@ -3,41 +3,46 @@ import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location'
 
+const WEATHER_API_KEY = "e523383c69b71446cff3758efc8f254d"
+const BASE_WEATHER_URL = "api.openweathermap.org/data/2.5/weather"
 export default function App() {
 
-  const [errorMessage, setErrorMessage] = useState(null)
+  const [errorMessage, setErrorMessage]=useState(null)
 
+  const [currentWeather, setCurrentWeather] = useState(null)
   useEffect(() =>{
-
-    console.log("hey")
-    const load = async () =>{
-      try{
-    console.log("hey2")
-
-        let {status} =  Location.requestForegroundPermissionsAsync()
-        if(status !== 'granted'){
-    console.log("hey5")
-
-          setErrorMessage('Access to location is needed to run the app')
-          return
-        }
-        console.log("hlo")
-
-        const location =  Location.getCurrentPositionAsync()
-        const {latitude, longitude} = location.coords
-        alert(`latitude: ${latitude}, longitude: ${longitude}` )
-        console.log(`latitude: ${latitude}, longitude: ${longitude}`)
-        
-
-      }catch(err){
-        console.log(err.message)
-      }
-
-    }
-
     load()
   }, [])
 
+  async function load(){
+    try {
+      let {status} = await Location.requestForegroundPermissionsAsync()
+      if(status !== 'granted'){
+        setErrorMessage('Access to location is allowed!')
+        return
+      }
+      const location =  await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+      console.log(location)
+      const {latitude, longitude} = location.coords
+      alert(`latitude ${latitude} and longitude: ${longitude}`)
+
+      const url = `${BASE_WEATHER_URL}?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`
+
+      const response = await fetch(url)
+
+      const result = await response.json()
+
+      if(response.ok){
+        setCurrentWeather(result)
+      }
+      else{
+        setErrorMessage(result.message)
+      }
+
+    }catch(error) {
+      console.log(error.message);
+   }
+  }
  
   return (
     <View style={styles.container}>
